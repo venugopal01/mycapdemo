983ca80260aaf6feb3bb11ca2f8fd84aa364fe42
venugopal01 <vg.adumulla@googlemail.com>
2022-03-24 06:20:27 +0000

    first commit

---
+++ app/capdemoui/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("ae.test.capdemoui.Component",{metadata:{manifest:"json"}})});
---
+++ app/services.cds
@@ -0,0 +1,2 @@
+
+using from './capdemoui/annotations';
